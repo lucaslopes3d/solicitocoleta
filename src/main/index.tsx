1a51d41928120393006bafba48eae84370ec1a72
Marcus-Silva-Oliveira <marcus.v.silvaoliveira1996@gmail.com>
2024-10-05 14:48:50 +0100

    Create SRC Folder

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import * as S from "./styles";
 import { ButtonComponent } from "../../components/ButtonComponent";
 import { TextInputTypes } from "./types";
-import { TextInputComponent } from "../../components/TextInputComponent";
-import { TextAreaInputComponent } from "../../components/TextAreaInputComponent";
-import { NumericInputComponent } from "../../components/NumericInputComponent";
 import { LogoComponent } from "../../components/LogoComponet";
 import collect from "@/api/routes/collect";
+import { TextInputComponent } from "@/components/textInputComponent";
+import { NumericInputComponent } from "@/components/numericInputComponent";
+import { TextAreaInputComponent } from "@/components/textAreaInputComponent";
 
 export const FormComponent: React.FC = () => {
   const [nome, setNome] = useState("");
